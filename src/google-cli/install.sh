f4cc4664bbe7a8d7de2f5ffa49dba7ffbe0eb0ca
Geoffroy <geoffroy.lindas@gmail.com>
2025-04-22 16:37:45 +0000

    base comit

---
+++ src/google-cli/install.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+VERSION=${VERSION:-"latest"}
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check_packages curl tar bash-completion
+
+install() {
+    local scriptTarFile=gcloudcli.zip
+
+    # See Linux install docs at https://cloud.google.com/sdk/docs/install
+    if [ "${VERSION}" != "latest" ]; then
+        local versionStr=-${VERSION}
+    fi
+    architecture=$(dpkg --print-architecture)
+    case "${architecture}" in
+        amd64) architectureStr=x86_64 ;;
+        i686) architectureStr=x86 ;;
+        arm64) architectureStr=aarch64 ;;
+        *)
+            echo "AWS CLI does not support machine architecture '$architecture'. Please use an x86, x86-64 or ARM64 machine."
+            exit 1
+    esac
+    local scriptUrl=https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli${versionStr}-linux-${architectureStr}.tar.gz
+    
+    curl "${scriptUrl}" -o "${scriptTarFile}"
+    curl "${scriptUrl}.sig" -o "${scriptSigFile}"
+
+    tar -xf "${scriptTarFile}"
+    ./google-cloud-sdk/install.sh --disable-prompts
+
+    rm -rf ./aws
+}
+
+echo "(*) Installing Google CLI..."
+
+install
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Done!"
